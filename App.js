d119178081f9c3271a73e3f0afd0f625f41abeef
Nishanth Shankar <nishanthShankr@gmail.com>
2021-09-25 07:29:28 +0530

    Google sign in done

---
+++ App.js
@@ -0,0 +1,166 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useState, useEffect} from 'react'
+import type {Node} from 'react'
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  Button,
+  View,
+} from 'react-native'
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen'
+import auth from '@react-native-firebase/auth'
+
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin'
+
+const GoogleSignIn = () => {
+  const onGoogleButtonPress = async () => {
+    GoogleSignin.configure()
+    // Get the users ID token
+    const {idToken} = await GoogleSignin.signIn()
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken)
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential)
+  }
+
+  return (
+    <Button
+      title='Google Sign-In'
+      onPress={() =>
+        onGoogleButtonPress().then(() => console.log('Signed in with Google!'))
+      }
+    />
+  )
+}
+
+const Section = ({children, title}): Node => {
+
+  
+
+  const isDarkMode = useColorScheme() === 'dark'
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  )
+}
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark'
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  }
+
+  const [initializing, setInitializing] = useState(true)
+  const [user, setUser] = useState()
+
+  // Handle user state changes
+  function onAuthStateChanged (user) {
+    setUser(user)
+    console.log('ISER1:', user.uid)
+    if (initializing) setInitializing(false)
+  }
+
+  useEffect(() => {
+    console.log('AUH:', auth)
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged)
+    // return subscriber // unsubscribe on unmount
+  }, [])
+
+  console.log('ISER:', user)
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View style={{height: 100}}></View>
+      <GoogleSignIn />
+      {/* <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView> */}
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+})
+
+export default App
